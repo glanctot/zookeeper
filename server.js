ca8e6cf612eb983b05f4d5cdaa10b8e78c25b2ba
glanctot <gregory.lanctot28@gmail.com>
2021-12-15 10:29:41 -0600

    added front end

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming json data
 app.use(express.json());
+
+app.use(express.static('public'));
 const {animals} = require('./data/animals.json');
 
 const PORT = process.env.PORT || 3001;
@@ -102,6 +104,22 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
